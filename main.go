f7b905383f5f20275504942f8e738bdcec603faf
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2020-10-16 14:59:30 +0200

    Update main.go
    Aggiunto piccolo ritardo alla risposta del servizio per fare un test

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 "net/http"
-
+"time"
 "github.com/go-chi/chi"
 "github.com/go-chi/chi/middleware"
 )
@@ -11,6 +11,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(15 * time.Second) 
 		w.Write([]byte("welcome"))
 	})
 	http.ListenAndServe(":3000", r)
