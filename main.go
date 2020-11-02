a9b5ef944cb81517f77879e41f18940779ee0083
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2020-11-02 16:42:18 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
-"net/http"
-"time"
-"github.com/go-chi/chi"
-"github.com/go-chi/chi/middleware"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"github.com/go-chi/chi"
 )
+
+var cont int64 = 1
+
+func getIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(15 * time.Second) 
-		w.Write([]byte("welcome"))
+		w.Write([]byte("welcome - local ip: "+getIP()+" - request number: "+strconv.FormatInt(cont, 10)))
+		cont++
 	})
 	http.ListenAndServe(":3000", r)
 }
