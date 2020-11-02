5e7d681464094895e67f457dcde32cbe3f3cc339
antoniocapizzi95 <antoniocapizzi95@hotmail.it>
2020-11-02 23:17:45 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"net"
 	"net/http"
 	"os"
 	"strconv"
@@ -9,12 +10,30 @@ import (
 
 var cont int64 = 1
 
+func getIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		os.Stderr.WriteString("Oops: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				os.Stdout.WriteString(ipnet.IP.String() + "\n")
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return ""
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome - server name: "+os.Getenv("SERVER")+" - request number: "+strconv.FormatInt(cont, 10)))
+		w.Write([]byte("welcome - local ip: "+getIP()+" - request number: "+strconv.FormatInt(cont, 10)))
 		cont++
 	})
 	http.ListenAndServe(":3000", r)
 }
+
 
